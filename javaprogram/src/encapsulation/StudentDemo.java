cddc60427dbfe61f8ee61c1c593285e9bee16c5d
STUDENT <STUDENT@DESKTOP-V5KJQDL>
2025-08-14 12:19:15 -0500

    Program for encapsulation

---
+++ javaprogram/src/encapsulation/StudentDemo.java
@@ -0,0 +1,24 @@
+package encapsulation;
+
+public class StudentDemo {
+	public static void main(String[] args) {
+		Student s=new Student();
+		s.setSid(101);
+		s.setSname("Rajan");
+		s.setAvg(97.5f);
+		
+		System.out.println("Student id:"+s.getSid());
+		System.out.println("Student name:"+s.getSname());
+		System.out.println("Student Average:"+s.getAvg());
+		
+		System.out.println(s); 
+		
+		Student s1=new Student();
+		s1.setSid(7);
+		s1.setSname("Dhoni");
+		s1.setAvg(100.0f);
+		
+		System.out.println(s1);
+	}
+
+}
